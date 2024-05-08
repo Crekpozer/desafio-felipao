1d74aa33bc950d089ee3919c6e40b0c6d5ce43ff
Crekpozer <ereck@live.com>
2024-05-08 11:52:10 -0300

    E as correções não param...

--- index.js
+++ index.js
@@ -3,26 +3,19 @@ let heroi = ["Anderson Souza", 6000]
 if (heroi[1] < 10001){
     if (heroi[1] < 1000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Ferro")
-    } 
-    if (heroi[1] > 1001 && heroi[1] < 2000){
+    } else if (heroi[1] > 1001 && heroi[1] < 2000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Bronze")
-    }
-    if (heroi[1] > 2001 && heroi[1] < 5000){
+    } else if (heroi[1] > 2001 && heroi[1] < 5000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Prata")
-    }
-    if (heroi[1] > 5001 && heroi[1] < 7000){
+    } else if (heroi[1] > 5001 && heroi[1] < 7000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Ouro")
-    }
-    if (heroi[1] > 7001 && heroi[1] < 8000){
+    } else if (heroi[1] > 7001 && heroi[1] < 8000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Prata")
-    }
-    if (heroi[1] > 8001 && heroi[1] < 9000){
+    } else if (heroi[1] > 8001 && heroi[1] < 9000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Prata")
-    }
-    if (heroi[1] > 9001 && heroi[1] < 10000){
+    } else if (heroi[1] > 9001 && heroi[1] < 10000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Prata")
-    }
-    if (heroi[1] > 2001 && heroi[1] < 5000){
+    } else if (heroi[1] > 2001 && heroi[1] < 5000){
         console.log("O Herói de nome " + heroi[0] + " está no nível de Prata")
     }
 } else {
